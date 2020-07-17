5e7fd2680eb3a10f389bd23457d5ae22e60e6a6c
Karim Kidiss <ozerioss@gmail.com>
2020-07-17 10:01:37 +0200

    Adds datasets & restructures code

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # WordTableParsing
 Parses Word documents tables
+
+
+Metadata in json to generate ?
--- main.py
+++ main.py
@@ -1,26 +1,49 @@
 from docx import Document
+import csv
+import pandas
 
 
 
-document = Document("wordTable.docx")
-table = document.tables[0]
+def readCsvColumns(filename):
+    with open('JDD/epkfdcpt.s5_0000121296_20200622_140027', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter = ';')
+        header = next(reader, None)
+    header
 
-data = []
+def readCsvColumnsPanda():
+    data = pandas.read_csv('JDD/epkfdcpt.s5_0000121296_20200622_140027', delimiter=";")
+    return data.columns
 
-keys = None
-for i, row in enumerate(table.rows):
-    text = (cell.text for cell in row.cells)
+def readWordTable(document):
+    document = Document(document)
+    table = document.tables[0]
 
-    # Establish the mapping based on the first row
-    # headers; these will become the keys of our dictionary
-    if i == 0:
-        keys = tuple(text)
-        continue
+    data = []
 
-    # Construct a dictionary for this row, mapping
-    # keys to values for this row
-    row_data = dict(zip(keys, text))
-    data.append(row_data)
+    keys = None
+    for i, row in enumerate(table.rows):
+        text = (cell.text for cell in row.cells)
+
+        # Establish the mapping based on the first row
+        # headers; these will become the keys of our dictionary
+        if i == 0:
+            keys = tuple(text)
+            continue
+
+        # Construct a dictionary for this row, mapping
+        # keys to values for this row
+        row_data = dict(zip(keys, text))
+        data.append(row_data)
 
 
-print(data[0])
+    # Loop on list and access fieldname
+    labelList = []
+    for label in data:
+        labelList.append(label['FIELDNAME'])
+
+    print(labelList)
+
+
+if __name__ == "__main__":
+    #readWordTable("wordTable.docx")
+    print(readCsvColumnsPanda())
